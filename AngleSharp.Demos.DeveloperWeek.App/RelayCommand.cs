f5ebfe2c9be26a9746ad842f4838c886e5a6eee2
Florian Rappl <rappl.florian@gmail.com>
2015-06-16 20:46:21 +0200

    Added relay command class

---
+++ AngleSharp.Demos.DeveloperWeek.App/RelayCommand.cs
@@ -0,0 +1,39 @@
+﻿namespace AngleSharp.Demos.DeveloperWeek
+{
+    using System;
+    using System.Windows.Input;
+
+    class RelayCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        Boolean _enabled;
+        Action _action;
+
+        public RelayCommand(Action action)
+        {
+            _enabled = true;
+        }
+
+        public Boolean IsEnabled
+        {
+            get { return _enabled; }
+            set
+            {
+                _enabled = value;
+                CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+            }
+        }
+
+        public Boolean CanExecute(Object parameter)
+        {
+            return _enabled;
+        }
+
+        public void Execute(Object parameter)
+        {
+            if (_enabled)
+                _action();
+        }
+    }
+}
