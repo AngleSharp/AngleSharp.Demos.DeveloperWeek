2e275b889263091a9c6b80ad2a6c4592bb18216e
Florian Rappl <rappl.florian@gmail.com>
2015-06-16 20:30:14 +0200

    Finished design and added base view model

---
+++ AngleSharp.Demos.DeveloperWeek.App/BaseViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace AngleSharp.Demos.DeveloperWeek
+{
+    using System;
+    using System.ComponentModel;
+    using System.Runtime.CompilerServices;
+
+    class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void TriggerChanged([CallerMemberName] String propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
