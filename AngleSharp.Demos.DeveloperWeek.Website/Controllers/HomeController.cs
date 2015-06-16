74b6f7fd1267c78ff726967c57bf7728f6eaf895
Florian Rappl <rappl.florian@gmail.com>
2015-06-16 17:34:36 +0200

    Prepared website for the demo

--- AngleSharp.Demos.DeveloperWeek.Website/Controllers/HomeController.cs
+++ AngleSharp.Demos.DeveloperWeek.Website/Controllers/HomeController.cs
@@ -1,10 +1,44 @@
 ﻿namespace AngleSharp.Demos.DeveloperWeek.Website.Controllers
 {
+    using AngleSharp.Demos.DeveloperWeek.Website.Models;
     using System.Web.Mvc;
+    using System.Web.Security;
 
     public class HomeController : Controller
     {
         public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult LogIn()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult LogIn(LogInModel model)
+        {
+            if (model.User == "User" && model.Password == "secret")
+            {
+                FormsAuthentication.SetAuthCookie(model.User, false);
+                return RedirectToAction("Index");
+            }
+
+            return View(model);
+        }
+
+        [HttpGet]
+        public RedirectToRouteResult LogOut()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("LogIn");
+        }
+
+        [HttpGet]
+        [Authorize]
+        public ViewResult Secret()
         {
             return View();
         }
---
+++ AngleSharp.Demos.DeveloperWeek.Website/Models/LogInModel.cs
@@ -0,0 +1,11 @@
+﻿namespace AngleSharp.Demos.DeveloperWeek.Website.Models
+{
+    using System;
+
+    public class LogInModel
+    {
+        public String User { get; set; }
+
+        public String Password { get; set; }
+    }
+}
