93b23fca678493f57354c45d2cde8837ea6c4e29
Florian Rappl <rappl.florian@gmail.com>
2015-06-16 11:12:50 +0200

    Initial commit; added demo app and website to deliver data

---
+++ AngleSharp.Demos.DeveloperWeek.Website/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿namespace AngleSharp.Demos.DeveloperWeek.Website.Controllers
+{
+    using System.Web.Mvc;
+
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
